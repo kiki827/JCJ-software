59baf41b2bd4b15d3f449ba2965136a52a109d04
tadoha <tdhl9927@icloud.com>
2017-07-11 14:48:31 +0900

    change
    New Change

---
+++ firstP/Assets/プログラム/NewBehaviourScript.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Update () {
+// suihei move
+transform.rotation *=
+Quaternion.AngleAxis(Input.GetAxis("Horizontal") * 50.0 * Time.deltaTime, Vector3(0,0,-1));
+// suichok move 
+transform.rotation *=
+Quaternion.AngleAxis(Input.GetAxis("Vertical") * 50.0 * Time.deltaTime, Vector3(1,0,0));
+
+if (Input.GetKey(KeyCode.Escape)) {
+Application.Quit();
+}
+
+
+}
---
+++ firstP/Assets/プログラム/SceneChange.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange : MonoBehaviour
+{
+
+    //ここは何も書かない
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Submit、つまりEnterキーでシーンを変更
+        if (Input.GetButtonDown("Submit"))
+        {
+            Application.LoadLevel("3Dball");
+        }
+    }
+}
