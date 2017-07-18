cdee2c0bef56496af7014cce077a5c99d8935a0d
C0116209 <c0116209e8@edu.teu.ac.jp>
2017-07-18 13:22:44 +0900

    First_commit
    7/18に貰った

---
+++ New Unity Project 1/Assets/NewBehaviourScript.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+// suihei move
+transform.rotation *=
+Quaternion.AngleAxis(Input.GetAxis("Horizontal") * 30.0 * Time.deltaTime, Vector3(0,0,-1));
+// suichok move 
+transform.rotation *=
+Quaternion.AngleAxis(Input.GetAxis("Vertical") * 30.0 * Time.deltaTime, Vector3(1,0,0));
+
+
+
+
+}
